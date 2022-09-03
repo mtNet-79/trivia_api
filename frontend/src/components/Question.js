bd2c2fedac1b6ab2794abe5155c7fa9ff76bbdcc
Michael Thornton <mp.thornton12@gmail.com>
2022-09-03 15:21:30 -0600

    updated to RR v6

--- frontend/src/components/Question.js
+++ frontend/src/components/Question.js
@@ -1,55 +1,46 @@
-import React, { Component } from 'react';
-import '../stylesheets/Question.css';
+import { useState } from "react";
+import "../stylesheets/Question.css";
 
-class Question extends Component {
-  constructor() {
-    super();
-    this.state = {
-      visibleAnswer: false,
-    };
-  }
+const Question = (props) => {
+  const [visibleAnswer, setVisibileAnswer] = useState(false);
+  const { question, answer, category, difficulty, questionAction } = props;
 
-  flipVisibility() {
-    this.setState({ visibleAnswer: !this.state.visibleAnswer });
-  }
-
-  render() {
-    const { question, answer, category, difficulty } = this.props;
-    return (
-      <div className='Question-holder'>
-        <div className='Question'>{question}</div>
-        <div className='Question-status'>
-          <img
-            className='category'
-            alt={`${category.toLowerCase()}`}
-            src={`${category.toLowerCase()}.svg`}
-          />
-          <div className='difficulty'>Difficulty: {difficulty}</div>
-          <img
-            src='delete.png'
-            alt='delete'
-            className='delete'
-            onClick={() => this.props.questionAction('DELETE')}
-          />
-        </div>
-        <div
-          className='show-answer button'
-          onClick={() => this.flipVisibility()}
-        >
-          {this.state.visibleAnswer ? 'Hide' : 'Show'} Answer
-        </div>
-        <div className='answer-holder'>
-          <span
-            style={{
-              visibility: this.state.visibleAnswer ? 'visible' : 'hidden',
-            }}
-          >
-            Answer: {answer}
-          </span>
-        </div>
+  const flipVisibility = () => {
+    // this.setState({ visibleAnswer: !this.state.visibleAnswer });
+    setVisibileAnswer(!visibleAnswer);
+  };
+ 
+  return (
+    <div className="Question-holder">
+      <div className="Question">{question}</div>
+      <div className="Question-status">
+        <img
+          className="category"
+          alt={`${category.toLowerCase()}`}
+          src={`${category.toLowerCase()}.svg`}
+        />
+        <div className="difficulty">Difficulty: {difficulty}</div>
+        <img
+          src="delete.png"
+          alt="delete"
+          className="delete"
+          onClick={questionAction("DELETE")}
+        />
       </div>
-    );
-  }
-}
+      <div className="show-answer button" onClick={flipVisibility()}>
+        {visibleAnswer ? "Hide" : "Show"} Answer
+      </div>
+      <div className="answer-holder">
+        <span
+          style={{
+            visibility: visibleAnswer ? "visible" : "hidden",
+          }}
+        >
+          Answer: {answer}
+        </span>
+      </div>
+    </div>
+  );
+};
 
 export default Question;
