b7c4d8eee382232fee66a0a4761f655aafc255f3
Michael Thornton <mp.thornton12@gmail.com>
2022-11-07 21:39:18 -0700

    PageNotFound edit

--- frontend/src/components/PageNotFound.js
+++ frontend/src/components/PageNotFound.js
@@ -2,12 +2,7 @@ import React, { Component } from "react";
 // import '../stylesheets/Question.css';
 
 class PageNotFound extends Component {
-  constructor() {
-    super();
-    // this.state = {
-    //   visibleAnswer: false,
-    // };
-  }
+
 
   render() {
     return <div className="NotFound">Not Found</div>;
